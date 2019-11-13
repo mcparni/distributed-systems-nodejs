658764018710d8d1245b1cbd675673c1e8b3c075
Miika Hyttinen <hyttinenmiika@gmail.com>
2019-11-13 15:36:56 +0200

    Random messages test

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # Distributed Systems NodeJS
+
 You need to have <a href="https://nodejs.org/en/">NodeJS</a> installed.
 
 ## Installation
 
 1. Clone the repo:  
-```$ git clone https://github.com/mcparni/distributed-systems-nodejs.git```  
+   `$ git clone https://github.com/mcparni/distributed-systems-nodejs.git`
 2. Change into directory:  
-```$ cd distributed-systems-nodejs```  
+   `$ cd distributed-systems-nodejs`
 3. Install dependencies:  
-```$ npm install``` 
+   `$ npm install`
 4. Open three terminal windows/tabs on current directory.
-  * Window 1: ```$ node index.js 1 3000```  
-  * Window 2: ```$ node index.js 2 3001```  
-  * Window 3: ```$ node index.js 3 3003```
-5. Send messages between clients.`  
+
+- Window 1: `$ node index.js 1 3000`
+- Window 2: `$ node index.js 2 3001`
+- Window 3: `$ node index.js 3 3003`
+
+5. Send messages between clients.`
+
+## Test with random
+
+Open three terminal windows/tabs on current directory.
+
+- Window 1: `$ node index.js 1 3000`
+- Window 2: `$ node index.js 2 3001`
+- Window 3: `$ node index.js 3 3003 tester`
+
+Change test configurations (payload and number of messages) in index.js file.
--- index.js
+++ index.js
@@ -1,65 +1,126 @@
 const express = require('express')
-const app = express();
-const axios = require('axios');
-const bodyParser  = require('body-parser');
-const readline = require("readline");
+const app = express()
+const axios = require('axios')
+const bodyParser = require('body-parser')
+const readline = require('readline')
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-const args = process.argv.slice(2);
-const client = args[0];
-const port = args[1];
+  input: process.stdin,
+  output: process.stdout
+})
+const args = process.argv.slice(2)
+const client = args[0]
+const port = args[1]
+//Open two clients without and one with this argument to set 'true'
+const testerClient = args[2]
 
-const node1Url = "http://localhost:3000/";
-const node2Url = "http://localhost:3001/";
-const node3Url = "http://localhost:3002/";
+// Test messages configuration
+const base = 'WHAT A COOL COURSE THIS IS'
+const numberOfMsg = 50
+const payload = 10
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const node1Url = 'http://localhost:3000/'
+const node2Url = 'http://localhost:3001/'
+const node3Url = 'http://localhost:3002/'
 
+let START_TIME
+let END_TIME
 
-app.post('/', (req, res) => {  
-  console.log(req.body.msg);
-  //res.send(req.body.msg);
-  return res.sendStatus(200);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body.msg)
+  if (req.body.msg.includes('START')) {
+    START_TIME = process.hrtime()
+  }
+  if (req.body.msg.includes('END')) {
+    logTestResult()
+  }
+  return res.sendStatus(200)
 })
 
-postToNode1 = (msg) => {
-  return axios.post(node1Url, { msg: msg },{'X-Requested-With': 'XMLHttpRequest'}); 
+const logTestResult = () => {
+  END_TIME = process.hrtime()
+  console.log(`<---- TEST RESULT ----> \n 
+              Test for ${numberOfMsg} messages with payload of ${payload} took \n
+              ${END_TIME[0] * 1000 +
+                END_TIME[1] / 1000000 -
+                START_TIME[0] * 1000 +
+                START_TIME[1] / 1000000} millisecond`)
 }
 
-postToNode2 = (msg) => {
-  return axios.post(node2Url, { msg: msg },{'X-Requested-With': 'XMLHttpRequest'}); 
+postToNode1 = msg => {
+  return axios.post(
+    node1Url,
+    { msg: msg },
+    { 'X-Requested-With': 'XMLHttpRequest' }
+  )
 }
 
-postToNode3 = (msg) => {
-  return axios.post(node3Url, { msg: msg },{'X-Requested-With': 'XMLHttpRequest'}); 
+postToNode2 = msg => {
+  return axios.post(
+    node2Url,
+    { msg: msg },
+    { 'X-Requested-With': 'XMLHttpRequest' }
+  )
 }
 
-const send = (msg) => {
-  axios.all([postToNode1(msg), postToNode2(msg), postToNode3(msg)])
-  .then(responseArr => {
-    //this will be executed only when all requests are complete
-   // console.log(': ', responseArr[0]);
-   // console.log(': ', responseArr[1]);
-   // console.log(': ', responseArr[2]);
-  })
-  .catch((error) => {
-    //console.error(error);
-  })
+postToNode3 = msg => {
+  return axios.post(
+    node3Url,
+    { msg: msg },
+    { 'X-Requested-With': 'XMLHttpRequest' }
+  )
 }
 
+const send = msg => {
+  axios
+    .all([postToNode1(msg), postToNode2(msg), postToNode3(msg)])
+    .then(responseArr => {
+      //this will be executed only when all requests are complete
+      // console.log(': ', responseArr[0]);
+      // console.log(': ', responseArr[1]);
+      // console.log(': ', responseArr[2]);
+    })
+    .catch(error => {
+      //console.error(error);
+    })
+}
 
-
-rl.on('line', function (msg) {
-  if(msg.toLowerCase() === "quit") {
-    console.log('Exit');
-    process.exit(0);
-  } else {
-      send(`from client ${client} :${msg}`);
+const generateRandomMessages = (base, length, numberOfMsg) => {
+  let messages = ['START']
+  console.log('MESSAGE nro 1 --->', messages[0])
+  while (messages.length < numberOfMsg) {
+    let message = ''
+    for (let i = 0; i < length; i++) {
+      message = message.concat(
+        base
+          .split('')
+          .sort(() => {
+            return 0.5 - Math.random()
+          })
+          .join('')
+      )
+    }
+    messages = messages.concat(message)
   }
-});
+  return messages.concat('END')
+}
+
+if (testerClient === 'true') {
+  const randomMessage = generateRandomMessages(base, payload, numberOfMsg)
+  randomMessage.forEach(msg => {
+    send(`from client ${client} :${msg}`)
+  })
+} else {
+  rl.on('line', function(msg) {
+    if (msg.toLowerCase() === 'quit') {
+      process.exit(0)
+    } else {
+      send(`from client ${client} :${msg}`)
+    }
+  })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
