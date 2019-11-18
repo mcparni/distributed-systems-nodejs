fc34c4cabce046cda821d37751fcada8c649605b
Miika Hyttinen <hyttinenmiika@gmail.com>
2019-11-18 21:57:46 +0200

    msg counter added

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ const port = args[1]
 const testerClient = args[2]
 
 // Test messages configurations
-const base = 'WHAT A COOL COURSE THIS IS'
-const numberOfMsg = 50
+const base = 'ABC'
+// 1-500
+const numberOfMsg = 500
+// 1-1000
 const payload = 10
 
 const node1Url = 'http://localhost:3000/'
@@ -24,24 +26,50 @@ const node3Url = 'http://localhost:3002/'
 
 let START_TIME
 let END_TIME
+let MSG_COUNTER = 0
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const generateRandomMessages = (base, length) => {
+  let messages = []
+  let numerator = 0
+  while (messages.length < 1000) {
+    let message = ''
+    numerator++
+    for (let i = 0; i < length; i++) {
+      message = message.concat(
+        base
+          .split('')
+          .sort(() => {
+            return 0.5 - Math.random()
+          })
+          .join('')
+      )
+    }
+    messages = messages.concat(message.concat(` MSG #: ${numerator}`))
+  }
+  return messages
+}
+
+const randomMessages = generateRandomMessages(base, payload)
+
 app.post('/', (req, res) => {
+  MSG_COUNTER++
   console.log(req.body.msg)
-  if (req.body.msg.includes('START')) {
+  if (MSG_COUNTER === 1) {
     START_TIME = process.hrtime()
   }
-  if (req.body.msg.includes('END')) {
+  if (MSG_COUNTER === numberOfMsg) {
     logTestResult()
+    process.exit()
   }
   return res.sendStatus(200)
 })
 
 const logTestResult = () => {
   END_TIME = process.hrtime()
-  console.log(`<---- TEST RESULT ----> \n 
+  console.log(`                        <---- TEST RESULT ----> \n 
               Test with ${numberOfMsg} messages and payload of ${payload} took \n
               ${END_TIME[0] * 1000 +
                 END_TIME[1] / 1000000 -
@@ -87,29 +115,8 @@ const send = msg => {
     })
 }
 
-const generateRandomMessages = (base, length, numberOfMsg) => {
-  let messages = ['START']
-  console.log('MESSAGE nro 1 --->', messages[0])
-  while (messages.length < numberOfMsg) {
-    let message = ''
-    for (let i = 0; i < length; i++) {
-      message = message.concat(
-        base
-          .split('')
-          .sort(() => {
-            return 0.5 - Math.random()
-          })
-          .join('')
-      )
-    }
-    messages = messages.concat(message)
-  }
-  return messages.concat('END')
-}
-
 if (testerClient === 'tester') {
-  const randomMessage = generateRandomMessages(base, payload, numberOfMsg)
-  randomMessage.forEach(msg => {
+  randomMessages.forEach(msg => {
     send(`from client ${client} :${msg}`)
   })
 } else {
